7547d88f25e010db5ae8f1a179818352f1d222e9
Jason <jasonandkhangminh@gmail.com>
2025-11-16 17:06:36 -0800

    Added login logic

--- ReportSystemManagement/ReportSystemManagement/Login.cs
+++ ReportSystemManagement/ReportSystemManagement/Login.cs
@@ -20,9 +20,20 @@ namespace ReportSystemManagement
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string username = username_input_box.Text;
+            string password = password_input_box.Text;
+
+            // In case one of inputs is empty
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter in both username and password", "Invalid Input(s)", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // Start login.py
             var start = new ProcessStartInfo();
             start.FileName = @"C:\Program Files\Python310\python.exe";
-            start.Arguments = $"login.py {username_input_box.Text} {password_input_box.Text}";
+            start.Arguments = $"login.py {username} {password}";
 
             start.UseShellExecute = false;
             start.CreateNoWindow = true;
@@ -31,20 +42,29 @@ namespace ReportSystemManagement
 
             using (Process process = Process.Start(start))
             {
-                String output = process.StandardOutput.ReadToEnd();
-                String error = process.StandardError.ReadToEnd();
+                string output = process.StandardOutput.ReadToEnd().ToString();
+                string error = process.StandardError.ReadToEnd();
 
-                if (!string.IsNullOrEmpty(error))
+                if (string.IsNullOrEmpty(error))
                 {
-                    MessageBox.Show(output, "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    if (output.Equals("Logged In"))
+                    MessageBox.Show(output);
+                    if (String.Equals(output, "Admin"))
                     {
+                        MessageBox.Show("You are logged in as ADMIN", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         Form mainForm = new Main_Page();
                         mainForm.Show();
                     }
-                } else
+
+                    else
+                    {
+                        MessageBox.Show("Incorrect username and/or password", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        MessageBox.Show(output);
+                        MessageBox.Show(error);
+                    }
+                }
+                else
                 {
-                    MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(error);
                 }
             }
         }
